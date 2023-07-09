e5045c6e95a6fe81e24d6d23501c418d7008b127
yhd1101 <asd159369@naver.com>
2023-07-09 10:10:09 +0900

    generate_crud_product

---
+++ src/product/product.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
+
+
+@Module({
+  imports : [],
+  controllers: [ProductController],
+  providers: [ProductService]
+})
+export class ProductModule {}
