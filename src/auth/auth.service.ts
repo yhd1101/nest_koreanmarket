13491ddbadf19d44da30eb2bad05ce5fea98294a
yhd1101 <asd159369@naver.com>
2023-07-16 11:02:30 +0900

    패스워드 암호화

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,17 +10,23 @@ export class AuthService {
   //회원가입 로직
   async createUser(createUserDto: CreateUserDto) {
     const user = await this.usersService.CreateUser(createUserDto);
+    user.password = undefined; //패스워드를 가려줌 (postman에 안보여짐)
     return user;
   }
 
   async Login(loginUserDto: LoginUserDto) {
     const user = await this.usersService.getUserByEmail(loginUserDto.email);
-    if (user.password !== loginUserDto.password) {
+    const isPasswordMatched = await user.validatePassword(
+      loginUserDto.password,
+    ); //성공여부
+
+    if (!isPasswordMatched) {
       throw new HttpException(
         'Password do not matched',
         HttpStatus.BAD_REQUEST,
       );
     } //email에서 입력한 패스워드랑 우리가 찾는 패스워드가 같지 않으면
+    user.password = undefined;
     return user;
   }
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,11 @@
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
 import { CommonEntity } from '../../common/entities/common.entity';
+import {
+  HttpException,
+  HttpStatus,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcryptjs'; //자바스크립트라이브러리를 가져와야해서 * as를 해줌
 
 @Entity()
 export class User extends CommonEntity {
@@ -9,4 +15,26 @@ export class User extends CommonEntity {
   public email: string;
   @Column()
   public password: string;
+
+  @BeforeInsert() //데이터를 넣기전에 실행하는 함수
+  async hashPassword() {
+    try {
+      //패스워드 암호화
+      const saltValue = await bcrypt.genSalt(10); //암호화되는 키값
+      this.password = await bcrypt.hash(this.password, saltValue);
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException();
+    }
+  }
+  //로그인할때 패스워드인증(암호화된패스워드를 맞는지 안맞는지 확인해줌)
+  async validatePassword(aPassword: string) {
+    try {
+      const isPasswordMatch = await bcrypt.compare(aPassword, this.password);
+      return isPasswordMatch;
+    } catch (err) {
+      console.log(err);
+      throw new HttpException(err.message, HttpStatus.CONFLICT);
+    }
+  }
 }
